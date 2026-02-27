e5dbf20029fa3ada392d2d51714a974a0671dadf
Alexandre Rafaschi <alexandre_rafasky@outlook.com>
2026-02-27 09:53:27 -0300

    wip

--- PrimeiroProjetoCsharp/Program.cs
+++ PrimeiroProjetoCsharp/Program.cs
@@ -3,7 +3,7 @@ using Destino;
 using ScreenSound;
 
 DesafioNumeroSecreto jogo = new DesafioNumeroSecreto();
-MenuInicial menu = new MenuInicial();
+MenuInicial menuScreenSound = new MenuInicial();
 HoraDaPratica teste = new HoraDaPratica();
 DicionarioAluno dicionarioAluno = new DicionarioAluno();
 EstoqueLoja estoque = new EstoqueLoja();
@@ -11,4 +11,4 @@ Quiz quiz = new Quiz();
 SistemaDeLogin login = new SistemaDeLogin();
 Carros carro = new Carros();
 
-carro.calculaMedia();
+parOuImpar.VerificarParesEImparesComForEach();
